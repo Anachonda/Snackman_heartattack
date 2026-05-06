7bdb04977fc818f00ffabd6dfb7059a8e0aa2306
Anachonda <f.j.vanweert@gmail.com>
2026-05-06 16:36:26 +0200

    Fix base path & workflow for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Snackman_heartattack/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
